aff83bf8581210ebaadbb6c721d97c658886af9c
ravanmaxmud <ravanim@code.edu.az>
2022-06-06 00:03:50 +0400

    Restore Mezenne Hesablama

--- Mezenne Hesablama/Mezenne Hesablama/Program.cs
+++ Mezenne Hesablama/Mezenne Hesablama/Program.cs
@@ -1,14 +1,16 @@
-﻿string command1 ="/show-recent-currency-rates";
+﻿string command1 = "/show-recent-currency-rates";
 string command2 = "/find-currency-rate-by-code";
 string command3 = "/calculate-amount-by-currecy";
 Console.WriteLine(command1);
-Console.WriteLine(command2); 
+Console.WriteLine(command2);
 Console.WriteLine(command3);
+string[] currencyValue = { "USD", "EUR", "TRY", "RUB" };
+float[] currencyvaluecost = { 1.7f, 1.8f, 0.1031f, 0.0271f };
 Console.WriteLine();
+double number = 0;
+
 while (true)
 {
-    string[] currencyValue = { "USD", "EUR","TRY","RUB"};
-    float[] currencyvaluecost = { 1.7f, 1.8f, 0.1031f, 0.0271f };
     int indexValue = 0;
     Console.Write($"Istediyinizi daxil edin:");
     string requierdName = Console.ReadLine();
@@ -23,7 +25,7 @@ while (true)
         {
             Console.Write("Choose the Currency :");
             string valueName = Console.ReadLine();
-
+            int totalcurrencycounter = 0;
             if (valueName == currencyValue[0])
             {
                 Console.WriteLine($"{currencyvaluecost[0]}AZN");
@@ -32,49 +34,30 @@ while (true)
             {
                 Console.WriteLine($"{currencyvaluecost[1]}AZN");
             }
-            if (valueName ==currencyValue[2])
+            if (valueName == currencyValue[2])
             {
                 Console.WriteLine($"{currencyvaluecost[2]}AZN");
             }
-            if (valueName ==currencyValue[3])
+            if (valueName == currencyValue[3])
             {
                 Console.WriteLine($"{currencyvaluecost[3]}AZN");
             }
-            
         }
-        //if (requierdName == command3)
-        //{
-        //    Console.Write("Calculate the Currency :");
-        //    string valueName = Console.ReadLine();
-        //    for (int i = 0; i <= 1000; i++)
-        //    {
-        //        if (i == currencyvaluecost[0])
-        //        {
-        //            Console.WriteLine(i*currencyValue.Length[0]);
-        //        }
-        //    }
-             
-        //}
-        indexValue++;
-    }   
+        if (requierdName == command3)
+        {
+            Console.Write($"Enter amount : (AZN) "); double requiredAZN = Convert.ToDouble(Console.ReadLine());
+            while (number <= 1000)
+            {
+                if (requiredAZN == number)
+                {
+                    Console.WriteLine(0.5882 * requiredAZN + " (USD)");
+                    Console.WriteLine(36.9003 * requiredAZN + " (RUB)");
+                    Console.WriteLine(9.6993 * requiredAZN + " (TRY)");
+                    Console.WriteLine(0.55 * requiredAZN + "(eur)");
+                }
+                number++;
+            }
+
+        }  indexValue++;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
